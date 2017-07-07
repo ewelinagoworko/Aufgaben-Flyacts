5c9acb9934b2d6b458e6c8c4f849a13f01934bed
Ewelina Goworko <ewelina.goworko@gmail.com>
2017-07-07 18:56:11 +0200

    commit

--- Aufgabe 2/js/main.js
+++ Aufgabe 2/js/main.js
@@ -1,16 +1,19 @@
 var slideIndex = 0;
-slider();
-
-function slider() {
-    var i;
-    var x = document.getElementsByClassName("part");
-    for (i = 0; i < x.length; i++) {
-      x[i].style.display = "none"; 
+    slider();
+    function slider() {
+        var i;
+        var x = document.getElementsByClassName("part");
+        for (i = 0; i < x.length; i++) {
+            x[i].classList.remove("slide");
+            x[i].classList.remove("slideOut");
+            x[i].className += " slideOut";
+        }
+        slideIndex++;
+        if (slideIndex > x.length) {
+            slideIndex = 1
+        }
+        x[slideIndex - 1].classList.remove("slideOut");
+        x[slideIndex - 1].style.display = "inline";
+        x[slideIndex - 1].className += " slide";
+        setTimeout(slider, 1500);
     }
-    slideIndex++;
-    if (slideIndex > x.length) {slideIndex = 1} 
-    x[slideIndex-1].style.display = "inline"; 
-    setTimeout(slider, 3000);
-}
-
-
