a044e9605add39585c88ba02df7f701fe01ff188
Ewelina Goworko <ewelina.goworko@gmail.com>
2017-07-07 17:43:57 +0200

    commit

--- Aufgabe 2/js/main.js
+++ Aufgabe 2/js/main.js
@@ -12,3 +12,22 @@ function slider() {
     x[slideIndex-1].style.display = "inline"; 
     setTimeout(slider, 3000);
 }
+
+
+function getLeft() {
+  var left = document.getElementsByClassName('part').style.left;
+  return Number(left.substr(0, left.length - 2));   // .substr removes the 'px'
+}
+// Move thumbs to the left
+function left() {
+  var currentLeft = getLeft();
+  var newLeft = currentLeft -500;
+  document.getElementsByClassName('part').style.left = newLeft + 'px';
+}
+
+// Move thumbs to the right
+function right() {
+  var currentLeft = getLeft();
+  var newLeft = currentLeft +500;
+  document.getElementsByClassName('part').style.left = newLeft + 'px';
+}
