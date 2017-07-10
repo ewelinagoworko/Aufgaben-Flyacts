02897e27b767f21f6ee76c04e547bb11406acf99
Ewelina Goworko <ewelina.goworko@gmail.com>
2017-07-10 16:59:45 +0200

    commit

--- Aufgabe 2/js/main.js
+++ Aufgabe 2/js/main.js
@@ -14,6 +14,18 @@ var slideIndex = 0;
 		setMoveLeft();
 	};
 
+	var current = 0,
+    slides = document.getElementsByClassName("part");
+
+	setInterval(function() {
+  		for (var i = 0; i < slides.length; i++) {
+			slides[i].style.opacity = 0;
+  		}
+		
+  		current = (current != slides.length - 1) ? current + 1 : 0;
+  		slides[current].style.opacity = 1;
+		}, 3000);
+
 
     
     function slider() {
