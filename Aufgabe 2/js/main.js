7e340e71964a974a513b3f2993a614cd7d557973
Ewelina Goworko <ewelina.goworko@gmail.com>
2017-07-10 12:50:03 +0200

    commit

--- Aufgabe 2/js/main.js
+++ Aufgabe 2/js/main.js
@@ -15,5 +15,20 @@ var slideIndex = 0;
         x[slideIndex - 1].classList.remove("slideOut");
         x[slideIndex - 1].style.display = "inline";
         x[slideIndex - 1].className += " slide";
-        setTimeout(slider, 1500);
+        setTimeout(slider, 3000);
+		
     }
+
+var lazyLoad = function() {
+	
+	window.addEventListener('load', function(){
+    var allimages= document.getElementsByTagName('img');
+    for (var i=0; i<allimages.length; i++) {
+        if (allimages[i].getAttribute('data-src')) {
+            allimages[i].setAttribute('src', allimages[i].getAttribute('data-src'));
+        }
+    }
+}, false)
+	
+}
+
