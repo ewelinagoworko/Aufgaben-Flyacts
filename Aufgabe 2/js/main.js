815488c5493a491a2352f6efbccfb6dacad95f36
Ewelina Goworko <ewelina.goworko@gmail.com>
2017-07-10 12:52:19 +0200

    commit

--- Aufgabe 2/js/main.js
+++ Aufgabe 2/js/main.js
@@ -1,34 +1,31 @@
 var slideIndex = 0;
-    slider();
+    setTimeout(slider, 3000);
+
+    	var move_left = 0;
+
+	function setMoveLeft(){
+		if (move_left != (document.getElementsByClassName("part")[0].offsetWidth + 4)) {
+			move_left = document.getElementsByClassName("part")[0].offsetWidth + 4;
+		}
+	}
+	setMoveLeft();
+
+	window.onresize = function(event) {
+		setMoveLeft();
+	};
+
+    
     function slider() {
         var i;
         var x = document.getElementsByClassName("part");
         for (i = 0; i < x.length; i++) {
-            x[i].classList.remove("slide");
-            x[i].classList.remove("slideOut");
-            x[i].className += " slideOut";
         }
         slideIndex++;
-        if (slideIndex > x.length) {
-            slideIndex = 1
+        
+	if (slideIndex >= x.length) {
+            slideIndex = 0
         }
-        x[slideIndex - 1].classList.remove("slideOut");
-        x[slideIndex - 1].style.display = "inline";
-        x[slideIndex - 1].className += " slide";
+	document.getElementsByClassName("content-handler")[0].style.marginLeft = (-move_left*slideIndex)+"px"
+	    
         setTimeout(slider, 3000);
-		
-    }
-
-var lazyLoad = function() {
-	
-	window.addEventListener('load', function(){
-    var allimages= document.getElementsByTagName('img');
-    for (var i=0; i<allimages.length; i++) {
-        if (allimages[i].getAttribute('data-src')) {
-            allimages[i].setAttribute('src', allimages[i].getAttribute('data-src'));
-        }
-    }
-}, false)
-	
 }
-
